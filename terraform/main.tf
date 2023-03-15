a560f53575fa971ceada385358dba3a35a907234
root <root@ip-172-31-0-127.eu-west-2.compute.internal>
2023-03-15 05:56:19 +0000

    code added

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region = "eu-west-2"
+access_key = "AKIA22QPREKEY2PJPI7S"
+secret_key = "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"
+}
+
+resource "aws_instance" "vm" {
+ami = "ami-0055e70f580e9ae80"
+instance_type = "t2.micro"
+tags =  {
+Name= "instance "
+}
+}
